fde813cf679fdbaab39badcb4c0a2f4cb1be0965
Chaoying <1754129@gmail.com>
2022-12-22 22:51:01 +0800

    feat: 加载天地图作为底图

--- src/map2d.js
+++ src/map2d.js
@@ -1,18 +1,30 @@
-import { mapboxToken } from "./store/mapconfig";
+import { mapboxToken, mapStyle } from "./store/mapconfig";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-class Map2d { 
-    constructor() { 
-        this.map = null;
-    }
-    initMap() { 
-        mapboxgl.accessToken = mapboxToken;
-        this.map = new mapboxgl.Map({
-          container: "mapContainer",
-          style: "mapbox://styles/mapbox/streets-v11",
-          center: [103.811279, 1.345399],
-          zoom: 12,
-        });
-    }
+class Map2d {
+  constructor() {
+    this.map = null;
+  }
+  initMap() {
+    this.map = new mapboxgl.Map({
+      accessToken: mapboxToken,
+      antialias: true,
+      container: "mapContainer",
+      style: mapStyle,
+      center: [121.470176, 31.237046],
+      zoom: 12,
+      maxZoom: 17,
+      minZoom: 0,
+    });
+    var nav = new mapboxgl.NavigationControl({
+      showCompass: true,
+      showZoom: true,
+    });
+    this.map.addControl(nav, "top-right");
+    this.map.on("zoomend", () => {
+      console.log("当前地图级别", this.map.getZoom());
+    });
+  }
+  //打印当前地图级别
 }
 export default new Map2d();
--- src/store/mapconfig.js
+++ src/store/mapconfig.js
@@ -1,2 +1,33 @@
 export const mapboxToken =
   "pk.eyJ1IjoiY2hhb3lpbmciLCJhIjoiY2xhdWo3bjN1MDRxeTNuczZucjI1azZjMSJ9.dFC1tvRTGdqazZ6thJGWdg";
+export const tdtToken = "13aaaf7907b41c2e04034d430da1ce36";
+const tdtVec = `http://t0.tianditu.gov.cn/vec_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=w&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=tiles&tk=${tdtToken}`;
+const tdtVecAnn = `http://t0.tianditu.gov.cn/cva_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cva&STYLE=default&TILEMATRIXSET=w&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=tiles&tk=${tdtToken}`;
+// mapbox 地图配置项
+export const mapStyle = {
+  version: 8,
+  sources: {
+    tdtVec: {
+      type: "raster",
+      tiles: [tdtVec],
+      tileSize: 256,
+    },
+    tdtVecAnn: {
+      type: "raster",
+      tiles: [tdtVecAnn],
+      tileSize: 256,
+    },
+  },
+  layers: [
+    {
+      id: "tdtVec",
+      type: "raster",
+      source: "tdtVec",
+    },
+    {
+      id: "tdtVecAnn",
+      type: "raster",
+      source: "tdtVecAnn",
+    },
+  ],
+};
