0df58349bb41db498231c82140c4e04a9a4532d5
Chaoying <1754129@gmail.com>
2022-12-24 23:37:41 +0800

    fix: 代码格式化整理

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -7,9 +7,9 @@ import map2d from "../map2d";
 import { eventbus } from "../event";
 onMounted(() => {
   map2d.initMap();
-  eventbus.on("changeBaseMap", (baseMap) => { 
+  eventbus.on("changeBaseMap", (baseMap) => {
     console.log("底图切换成功", baseMap);
-  })
+  });
 });
 </script>
 <style lang="css">
